0b40860c76e26c66a3572f7c1b814d04684a6887
Rikku Miling <francis.miling@ciit.edu.ph>
2024-06-30 22:25:11 +0800

    Clean Up

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-// src/App.js
 import React from 'react';
-import { ThemeProvider } from './ThemeContext';
-import Header from './Header';
-import Content from './Content';
 import './App.css';
 
 function App() {
   return (
-      <ThemeProvider>
         <div className="App">
-          <Header />
-          <Content />
+
         </div>
-      </ThemeProvider>
   );
 }
 
